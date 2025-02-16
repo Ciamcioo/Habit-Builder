b7ea61eb9b585bdf4acb33c6d79feeca6f1ba053
Jakub Maciocha <ciamcio@dellg15jm.home>
2025-02-16 17:38:23 +0100

    Initial commit

---
+++ src/main/java/io/github/ciamcioo/habbit_builder/HabbitBuilderApplication.java
@@ -0,0 +1,13 @@
+package io.github.ciamcioo.habbit_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabbitBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabbitBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ciamcioo/habbit_builder/HabbitBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ciamcioo.habbit_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabbitBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
