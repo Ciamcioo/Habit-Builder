3bf73d9543342103a99d084fc57befed5f38e41b
Jakub Maciocha <ciamcio@dellg15jm.home>
2025-03-07 21:03:20 +0100

    Providing new API endpoints with custom error handling, used standarized approach to implementing API

--- src/main/java/io/github/ciamcioo/habit_builder/exceptions/RestResponseEntityExceptionHandler.java
+++ src/main/java/io/github/ciamcioo/habit_builder/exceptions/RestResponseEntityExceptionHandler.java
@@ -1,12 +1,21 @@
 package io.github.ciamcioo.habit_builder.exceptions;
 
+import jakarta.validation.ValidationException;
+import jakarta.xml.bind.ValidationEventHandler;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -14,16 +23,35 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     @ExceptionHandler(value = {
         HabitAlreadyExistsException.class
     })
-    protected ResponseEntity<Object> badRequestException(RuntimeException exception, WebRequest request) {
-        return handleExceptionInternal(exception, exception.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    protected ResponseEntity<Error> badRequestExceptionHandler(RuntimeException exception, WebRequest request) {
+        return new ResponseEntity<>(
+                new Error(exception.getMessage()),
+                HttpStatus.BAD_REQUEST
+        );
     }
 
     @ExceptionHandler(value = {
             HabitNotFoundException.class
     })
-    protected ResponseEntity<Object> notFoundException(RuntimeException exception, WebRequest request) {
-        return handleExceptionInternal(exception, exception.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    protected ResponseEntity<Error> notFoundExceptionHandler(RuntimeException exception, WebRequest request) {
+        return new ResponseEntity<>(
+                new Error(exception.getMessage()),
+                HttpStatus.NOT_FOUND
+        );
     }
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getAllErrors().forEach(error -> {
+            errors.put(
+                    ((FieldError) error).getField(),
+                    error.getDefaultMessage()
+            );
+        });
 
-
+        return new ResponseEntity<>(
+            errors,
+            HttpStatus.BAD_REQUEST
+        );
+    }
 }
